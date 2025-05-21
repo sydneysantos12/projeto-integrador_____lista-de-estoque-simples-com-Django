562a6d4286f2609eb09a9c437b5c3f58e6ac4980
sydneysantos12 <samuelsantos.sud07@gmail.com>
2025-05-21 17:30:14 -0300

    FINAL

--- lista/views.py
+++ lista/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
 
 from .models import Todo
 
-def todo_list(request):
-    todos = Todo.objects.all()
-    return render(request, "todos/todo_list.html", {"todos":todos})
+
+class TodoListView(ListView):
+    model = Todo
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.mark_has_complete()
+        return redirect("todo_list")
